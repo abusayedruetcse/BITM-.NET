8c7e959ff92de86638b0aa217964d063a86ca4dd
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-05-29 01:01:56 +0600

    Day5: Practice 1, Customer Information for Bank A/C

---
+++ MyWinApp/MyWinApp/BankAccount.Designer.cs
@@ -0,0 +1,409 @@
+﻿namespace MyWinApp
+{
+    partial class BankAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.accountNoLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.contactNoLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.contactNoTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.accountNoTextBox = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.accountNumberTextBox = new System.Windows.Forms.TextBox();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.DepositeButton = new System.Windows.Forms.Button();
+            this.WithdrawButton = new System.Windows.Forms.Button();
+            this.BalanceButton = new System.Windows.Forms.Button();
+            this.showRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Last Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Contact No.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(88, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(88, 245);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Address";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(88, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Account No.";
+            // 
+            // accountNoLabel
+            // 
+            this.accountNoLabel.AutoSize = true;
+            this.accountNoLabel.Location = new System.Drawing.Point(400, 278);
+            this.accountNoLabel.Name = "accountNoLabel";
+            this.accountNoLabel.Size = new System.Drawing.Size(0, 13);
+            this.accountNoLabel.TabIndex = 8;
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(394, 245);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(0, 13);
+            this.addressLabel.TabIndex = 9;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(394, 213);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(0, 13);
+            this.emailLabel.TabIndex = 10;
+            // 
+            // contactNoLabel
+            // 
+            this.contactNoLabel.AutoSize = true;
+            this.contactNoLabel.Location = new System.Drawing.Point(394, 180);
+            this.contactNoLabel.Name = "contactNoLabel";
+            this.contactNoLabel.Size = new System.Drawing.Size(0, 13);
+            this.contactNoLabel.TabIndex = 11;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(394, 148);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.lastNameLabel.TabIndex = 12;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(394, 114);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.firstNameLabel.TabIndex = 13;
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(394, 82);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.userNameLabel.TabIndex = 14;
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(177, 82);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(183, 20);
+            this.userNameTextBox.TabIndex = 15;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(177, 114);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(183, 20);
+            this.firstNameTextBox.TabIndex = 16;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(177, 148);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(183, 20);
+            this.lastNameTextBox.TabIndex = 17;
+            // 
+            // contactNoTextBox
+            // 
+            this.contactNoTextBox.Location = new System.Drawing.Point(177, 180);
+            this.contactNoTextBox.Name = "contactNoTextBox";
+            this.contactNoTextBox.Size = new System.Drawing.Size(183, 20);
+            this.contactNoTextBox.TabIndex = 18;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(177, 213);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(183, 20);
+            this.emailTextBox.TabIndex = 19;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(177, 245);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(183, 20);
+            this.addressTextBox.TabIndex = 20;
+            // 
+            // accountNoTextBox
+            // 
+            this.accountNoTextBox.Location = new System.Drawing.Point(177, 275);
+            this.accountNoTextBox.Name = "accountNoTextBox";
+            this.accountNoTextBox.Size = new System.Drawing.Size(183, 20);
+            this.accountNoTextBox.TabIndex = 21;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(177, 313);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 22;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(285, 313);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 23;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(612, 82);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(87, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Account Number";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(637, 187);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(43, 13);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Amount";
+            // 
+            // accountNumberTextBox
+            // 
+            this.accountNumberTextBox.Location = new System.Drawing.Point(564, 111);
+            this.accountNumberTextBox.Name = "accountNumberTextBox";
+            this.accountNumberTextBox.Size = new System.Drawing.Size(183, 20);
+            this.accountNumberTextBox.TabIndex = 26;
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(564, 213);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(183, 20);
+            this.amountTextBox.TabIndex = 27;
+            // 
+            // DepositeButton
+            // 
+            this.DepositeButton.Location = new System.Drawing.Point(564, 245);
+            this.DepositeButton.Name = "DepositeButton";
+            this.DepositeButton.Size = new System.Drawing.Size(75, 23);
+            this.DepositeButton.TabIndex = 28;
+            this.DepositeButton.Text = "Deposite";
+            this.DepositeButton.UseVisualStyleBackColor = true;
+            // 
+            // WithdrawButton
+            // 
+            this.WithdrawButton.Location = new System.Drawing.Point(672, 245);
+            this.WithdrawButton.Name = "WithdrawButton";
+            this.WithdrawButton.Size = new System.Drawing.Size(75, 23);
+            this.WithdrawButton.TabIndex = 29;
+            this.WithdrawButton.Text = "Withdraw";
+            this.WithdrawButton.UseVisualStyleBackColor = true;
+            // 
+            // BalanceButton
+            // 
+            this.BalanceButton.Location = new System.Drawing.Point(624, 278);
+            this.BalanceButton.Name = "BalanceButton";
+            this.BalanceButton.Size = new System.Drawing.Size(75, 23);
+            this.BalanceButton.TabIndex = 30;
+            this.BalanceButton.Text = "Balance";
+            this.BalanceButton.UseVisualStyleBackColor = true;
+            // 
+            // showRichTextBox
+            // 
+            this.showRichTextBox.Location = new System.Drawing.Point(91, 363);
+            this.showRichTextBox.Name = "showRichTextBox";
+            this.showRichTextBox.Size = new System.Drawing.Size(666, 111);
+            this.showRichTextBox.TabIndex = 31;
+            this.showRichTextBox.Text = "";
+            // 
+            // BankAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 486);
+            this.Controls.Add(this.showRichTextBox);
+            this.Controls.Add(this.BalanceButton);
+            this.Controls.Add(this.WithdrawButton);
+            this.Controls.Add(this.DepositeButton);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.accountNumberTextBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.accountNoTextBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.contactNoTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.contactNoLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.accountNoLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "BankAccount";
+            this.Text = "BankAccount";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label accountNoLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label contactNoLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox contactNoTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox accountNoTextBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox accountNumberTextBox;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.Button DepositeButton;
+        private System.Windows.Forms.Button WithdrawButton;
+        private System.Windows.Forms.Button BalanceButton;
+        private System.Windows.Forms.RichTextBox showRichTextBox;
+    }
+}
---
+++ MyWinApp/MyWinApp/BankAccount.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWinApp
+{
+    public partial class BankAccount : Form
+    {
+        List<string> userNames = new List<string>();
+        List<string> firstNames = new List<string>();
+        List<string> lastNames = new List<string>();
+        List<int> contactNos = new List<int>();
+        List<string> emails = new List<string>();
+        List<string> addresss = new List<string>();
+        List<int> accountNos = new List<int>();
+
+
+        public BankAccount()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string userName;
+                string firstName;
+                string lastName;
+                int contactNo;
+                string email;
+                string address;
+                int accountNo;
+
+                userName = userNameTextBox.Text;
+                firstName = firstNameTextBox.Text;
+                lastName = lastNameTextBox.Text;
+                contactNo = Convert.ToInt32(contactNoTextBox.Text);
+                email = emailTextBox.Text;
+                address = addressTextBox.Text;
+                accountNo = Convert.ToInt32(accountNoTextBox.Text);
+
+                userNames.Add(userName);
+                firstNames.Add(firstName);
+                lastNames.Add(lastName);
+                contactNos.Add(contactNo);
+                emails.Add(email);
+                addresss.Add(address);
+                accountNos.Add(accountNo);
+
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            showRichTextBox.Text = Display();
+        }
+        private string Display()
+        {
+            string message = "";
+            message = "SI\tUser Name:\tFirst Name\tLast Name\tContact No\tEmail\tAddress\tAccount No.\n";
+            for (int index = 0; index < userNames.Count; index++)
+            {
+                message += (index + 1) + "\t" + userNames[index] + "\t" + firstNames[index] + "\t" + lastNames[index] + "\t" + contactNos[index] + "\t" + emails[index] + "\t" + addresss[index] + "\t" + accountNos[index] + "\n";
+
+            }
+            return message;
+        }
+    }
+}
