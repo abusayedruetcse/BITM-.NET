d59ab7f2602e0cf8402fb30f969cf410073b7860
TUmi Koi <yourteacher@gmail.com>
2019-06-16 15:01:51 +0600

    Exception Handling in DB connection

--- MyWinApp/MyWinApp/DatabaseConnection.cs
+++ MyWinApp/MyWinApp/DatabaseConnection.cs
@@ -20,34 +20,42 @@ namespace MyWinApp
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
-            //1
-            SqlConnection sqlConnection = new SqlConnection();
-            String connectionString = @"Server=PC-301-17\SQLEXPRESS ;Database=StudentDB ; Integrated Security=True ";
-            sqlConnection.ConnectionString = connectionString;
-
-            //2
-            SqlCommand sqlCommand = new SqlCommand();
-            String commandString = @"INSERT INTO Students(RollNo,Name,Age,Address,DistrictID) VALUES('CSE001', 'ALI', 21, 'Mirpur', 1)";
-            sqlCommand.CommandText = commandString;
-            sqlCommand.Connection = sqlConnection;
-
-            //3
-            sqlConnection.Open();
-
-            //4
-            int isExecuted = 0;
-            isExecuted=sqlCommand.ExecuteNonQuery();
-            if(isExecuted>0)
+            try
             {
-                MessageBox.Show("Saved Successfully");
-            }
-            else
-            {
-                MessageBox.Show("Save Failed!");
-            }
+                //1
+                SqlConnection sqlConnection = new SqlConnection();
+                String connectionString = @"Server=PC-301-17\SQLEXPRESS ;Database=StudentDB ; Integrated Security=True ";
+                sqlConnection.ConnectionString = connectionString;
 
-            //5
-            sqlConnection.Close();
+                //2
+                SqlCommand sqlCommand = new SqlCommand();
+                String commandString = @"INSERT INTO Students(RollNo,Name,Age,Address,DistrictID) VALUES('CSE001', 'ALI', 21, 'Mirpur', 1)";
+                sqlCommand.CommandText = commandString;
+                sqlCommand.Connection = sqlConnection;
+
+                //3
+                sqlConnection.Open();
+
+                //4
+                int isExecuted = 0;
+                isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    MessageBox.Show("Saved Successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Save Failed!");
+                }
+
+                //5
+                sqlConnection.Close();
+            }
+            catch (Exception excetion)
+            {
+                MessageBox.Show(excetion.Message);
+            }
+            
 
 
         }
