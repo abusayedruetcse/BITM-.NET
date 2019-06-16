de7a998676cfe37de231621b5b18ed1358e05143
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-06-17 01:08:16 +0600

    Insert method built for CategorySetup Form

--- StockManagementSystem/StockManagementSystem/CategorySetup.cs
+++ StockManagementSystem/StockManagementSystem/CategorySetup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,51 @@ namespace StockManagementSystem
         {
             InitializeComponent();
         }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string name = "";
+            name = nameTextBox.Text;
+            Insert(name);
+        }
+        private void Insert(string name)
+        {
+            try
+            {
+                //1
+                string connectionString = @"Server=DESKTOP-AAHS936\SQLEXPRESS;Database=StockManagementDB;Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = connectionString;
+
+                //2
+                string commandString = @"INSERT INTO Categories (Name) VALUES('"+name+"')";
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = commandString;
+                sqlCommand.Connection = sqlConnection;
+
+                //3
+                sqlConnection.Open();
+
+                //4
+                int isExecuted = 0;
+                isExecuted= sqlCommand.ExecuteNonQuery(); 
+                if (isExecuted>0)
+                {
+                    MessageBox.Show("Saved Successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Save Failed!");
+                }
+                //5
+                sqlConnection.Close();
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+        
     }
 }
--- StockManagementSystem/StockManagementSystem/Program.cs
+++ StockManagementSystem/StockManagementSystem/Program.cs
@@ -16,12 +16,13 @@ namespace StockManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new CategorySetup());
+            //Application.Run(new FeatureList());
+            Application.Run(new CategorySetup());
             //Application.Run(new CompanySetup());
             //Application.Run(new ItemSetup());
             //Application.Run(new StockIn());
             //Application.Run(new StockOut());
-            Application.Run(new SearchandViewItemsSummary());
+            //Application.Run(new SearchandViewItemsSummary());
             //Application.Run(new ViewBetweenTwoDatesReport());
         }
     }
