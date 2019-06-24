4a7f2dced707e580308e1764b28570694457d293
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-06-24 23:28:09 +0600

    Database Script fom CustomerAccountSystem

---
+++ CustomerAccountSystem/CustomerInfoDB.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE CustomerInfoDB
+
+USE CustomerInfoDB
+
+CREATE TABLE Customers
+(
+AccountNo VARCHAR(20),
+Name VARCHAR(35),
+Email VARCHAR(40),
+OpeningDate VARCHAR(20)
+)
+-- DROP TABLE Customers
+DELETE Customers
+
+CREATE TABLE Accounts
+(
+AccountNo VARCHAR(20),
+Amount NUMERIC(15,2)
+)
+-- DROP TABLE Accounts
+SELECT * FROM Customers WHERE AccountNo= '1223'
+INSERT INTO Customers VALUES ('1223','Jahid','adfa@gmail.com','12/2/19')
+SELECT * FROM Accounts WHERE AccountNo= ''
+UPDATE Accounts SET Amount=  WHERE AccountNo=''
+SELECT Name,c.AccountNo,OpeningDate,Amount AS Balance FROM Customers AS c LEFT JOIN Accounts AS a ON c.AccountNo=a.AccountNo WHERE c.AccountNo= '12345678'
