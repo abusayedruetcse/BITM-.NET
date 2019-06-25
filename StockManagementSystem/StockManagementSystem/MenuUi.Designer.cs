21a847afac46a18bfcc5230af327b99d5d1a8af8
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-06-25 10:45:36 +0600

    Connecting all form with Menu

--- StockManagementSystem/StockManagementSystem/MenuUi.Designer.cs
+++ StockManagementSystem/StockManagementSystem/MenuUi.Designer.cs
@@ -28,112 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.CategoryButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.categorySetupLabel = new System.Windows.Forms.Label();
+            this.companySetupLabel = new System.Windows.Forms.Label();
+            this.itemSetupLabel = new System.Windows.Forms.Label();
+            this.stockInLabel = new System.Windows.Forms.Label();
+            this.stockOutLabel = new System.Windows.Forms.Label();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.viewLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // CategoryButton
+            // categorySetupLabel
             // 
-            this.CategoryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CategoryButton.Location = new System.Drawing.Point(163, 70);
-            this.CategoryButton.Name = "CategoryButton";
-            this.CategoryButton.Size = new System.Drawing.Size(252, 30);
-            this.CategoryButton.TabIndex = 7;
-            this.CategoryButton.Text = "Category Setup";
-            this.CategoryButton.UseVisualStyleBackColor = true;
-            this.CategoryButton.Click += new System.EventHandler(this.CategoryButton_Click);
+            this.categorySetupLabel.AutoSize = true;
+            this.categorySetupLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categorySetupLabel.Location = new System.Drawing.Point(177, 48);
+            this.categorySetupLabel.Name = "categorySetupLabel";
+            this.categorySetupLabel.Size = new System.Drawing.Size(101, 16);
+            this.categorySetupLabel.TabIndex = 14;
+            this.categorySetupLabel.Text = "Category Setup";
+            this.categorySetupLabel.Click += new System.EventHandler(this.categorySetupLabel_Click);
             // 
-            // button1
+            // companySetupLabel
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(163, 125);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(252, 30);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Company Setup";
-            this.button1.UseVisualStyleBackColor = true;
+            this.companySetupLabel.AutoSize = true;
+            this.companySetupLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.companySetupLabel.Location = new System.Drawing.Point(177, 101);
+            this.companySetupLabel.Name = "companySetupLabel";
+            this.companySetupLabel.Size = new System.Drawing.Size(104, 16);
+            this.companySetupLabel.TabIndex = 15;
+            this.companySetupLabel.Text = "Company Setup";
+            this.companySetupLabel.Click += new System.EventHandler(this.companySetupLabel_Click);
             // 
-            // button2
+            // itemSetupLabel
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(163, 179);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(252, 30);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Item Setup";
-            this.button2.UseVisualStyleBackColor = true;
+            this.itemSetupLabel.AutoSize = true;
+            this.itemSetupLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemSetupLabel.Location = new System.Drawing.Point(177, 155);
+            this.itemSetupLabel.Name = "itemSetupLabel";
+            this.itemSetupLabel.Size = new System.Drawing.Size(71, 16);
+            this.itemSetupLabel.TabIndex = 16;
+            this.itemSetupLabel.Text = "Item Setup";
+            this.itemSetupLabel.Click += new System.EventHandler(this.itemSetupLabel_Click);
             // 
-            // button3
+            // stockInLabel
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(163, 235);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(252, 30);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "Stock In";
-            this.button3.UseVisualStyleBackColor = true;
+            this.stockInLabel.AutoSize = true;
+            this.stockInLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stockInLabel.Location = new System.Drawing.Point(177, 211);
+            this.stockInLabel.Name = "stockInLabel";
+            this.stockInLabel.Size = new System.Drawing.Size(55, 16);
+            this.stockInLabel.TabIndex = 17;
+            this.stockInLabel.Text = "Stock In";
+            this.stockInLabel.Click += new System.EventHandler(this.stockInLabel_Click);
             // 
-            // button4
+            // stockOutLabel
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(163, 291);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(252, 30);
-            this.button4.TabIndex = 11;
-            this.button4.Text = "Stock Out";
-            this.button4.UseVisualStyleBackColor = true;
+            this.stockOutLabel.AutoSize = true;
+            this.stockOutLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stockOutLabel.Location = new System.Drawing.Point(177, 264);
+            this.stockOutLabel.Name = "stockOutLabel";
+            this.stockOutLabel.Size = new System.Drawing.Size(65, 16);
+            this.stockOutLabel.TabIndex = 18;
+            this.stockOutLabel.Text = "Stock Out";
+            this.stockOutLabel.Click += new System.EventHandler(this.stockOutLabel_Click);
             // 
-            // button5
+            // searchLabel
             // 
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(163, 344);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(252, 30);
-            this.button5.TabIndex = 12;
-            this.button5.Text = "Search & View Items Summary";
-            this.button5.UseVisualStyleBackColor = true;
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchLabel.Location = new System.Drawing.Point(177, 308);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(178, 16);
+            this.searchLabel.TabIndex = 19;
+            this.searchLabel.Text = "Search View Items Summary";
+            this.searchLabel.Click += new System.EventHandler(this.searchLabel_Click);
             // 
-            // button6
+            // viewLabel
             // 
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(163, 399);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(252, 30);
-            this.button6.TabIndex = 13;
-            this.button6.Text = "View Between Two Dates Report";
-            this.button6.UseVisualStyleBackColor = true;
+            this.viewLabel.AutoSize = true;
+            this.viewLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewLabel.Location = new System.Drawing.Point(177, 349);
+            this.viewLabel.Name = "viewLabel";
+            this.viewLabel.Size = new System.Drawing.Size(204, 16);
+            this.viewLabel.TabIndex = 20;
+            this.viewLabel.Text = "View Between Two Dates Report";
+            this.viewLabel.Click += new System.EventHandler(this.viewLabel_Click);
             // 
-            // FeatureList
+            // MenuUi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(599, 470);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.CategoryButton);
-            this.Name = "FeatureList";
-            this.Text = "Feature List";
+            this.Controls.Add(this.viewLabel);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.stockOutLabel);
+            this.Controls.Add(this.stockInLabel);
+            this.Controls.Add(this.itemSetupLabel);
+            this.Controls.Add(this.companySetupLabel);
+            this.Controls.Add(this.categorySetupLabel);
+            this.Name = "MenuUi";
+            this.Text = "Menu List";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button CategoryButton;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label categorySetupLabel;
+        private System.Windows.Forms.Label companySetupLabel;
+        private System.Windows.Forms.Label itemSetupLabel;
+        private System.Windows.Forms.Label stockInLabel;
+        private System.Windows.Forms.Label stockOutLabel;
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.Label viewLabel;
     }
 }
