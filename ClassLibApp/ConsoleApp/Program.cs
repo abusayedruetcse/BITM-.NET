4979126e87fb677fc5cb424be1fb643389182560
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-07-10 08:33:32 +0600

    Connecting new ConsoleApp with BLL & Models

---
+++ ClassLibApp/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibApp.BLL.BLL;
+using ClassLibApp.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            StudentManager _studentManager = new StudentManager();
+            string Name = "Fatima";
+            string Roll = "12345";
+            Console.WriteLine(_studentManager.AddStudent(Name, Roll));
+            Console.ReadKey();
+        }
+    }
+}
