d38d3f77bc857e60124d93110fb981808a8f4abb
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-07-28 21:47:24 +0600

    MyMVCApp: 6. Repository added

---
+++ MyMVCApp/MyMVCApp.Repository/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using MyMVCApp.DatabaseContext.DatabaseContext;
+using MyMVCApp.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMVCApp.Repository.Repository
+{
+    public class CourseRepository
+    {
+        StudentDbContext db;
+        public CourseRepository()
+        {
+            db = new StudentDbContext();
+        }
+        public void Add(Course course)
+        {
+            db.Courses.Add(course);
+            db.SaveChanges();
+        }
+    }
+}
