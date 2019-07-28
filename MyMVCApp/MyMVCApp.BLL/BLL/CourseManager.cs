609f8e4c724272d2f6ffce6b87fce726c43a5934
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-07-28 21:50:16 +0600

    MyMVCApp: 7. BLL is added

---
+++ MyMVCApp/MyMVCApp.BLL/BLL/CourseManager.cs
@@ -0,0 +1,24 @@
+﻿using MyMVCApp.Models.Models;
+using MyMVCApp.Repository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMVCApp.BLL.BLL
+{
+    public class CourseManager
+    {
+        CourseRepository _courseRepository;
+        public CourseManager()
+        {
+            _courseRepository = new CourseRepository();
+        }
+        public void Add(Course course)
+        {
+            _courseRepository.Add(course);
+        }
+
+    }
+}
